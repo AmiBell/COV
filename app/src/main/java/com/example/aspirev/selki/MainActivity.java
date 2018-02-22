60384d424f0bcb9755c415ec4489d690954513be
lenovo <soniahassouna1@gmail.com>
2018-02-22 23:31:16 +0000

    Internationalisation

--- app/src/main/java/com/example/aspirev/selki/MainActivity.java
+++ app/src/main/java/com/example/aspirev/selki/MainActivity.java
@@ -53,39 +53,8 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_Contacter) {
-            Intent intent = new Intent(this,Contactez_nous.class);
-            startActivity(intent);
-            return true ;
-        }
-        if (id == R.id.action_chercher) {
-            Intent intent = new Intent(this,ChercherCovoiturage.class);
-            startActivity(intent);
-            return true;
-        }
-        if (id == R.id.action_proposer) {
-            Intent intent = new Intent(this,ProposerCovoiturage.class);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
